c1e26a40427bfae3395cee1c8b4a62f57d7e0409
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-23 19:02:10 +1000

    I love annotations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  bio        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Profile < ActiveRecord::Base
 end
--- test/models/skill_test.rb
+++ test/models/skill_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: skills
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  skill      :string
+#  system     :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class SkillTest < ActiveSupport::TestCase
