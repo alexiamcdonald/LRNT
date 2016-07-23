3e14b12879943a801c60ed3b86d4848b55c29269
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-23 17:01:32 +1000

    Change routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :profiles
-  root 'profiles#index'
-  get 'settings/view'
+  root 'settings#view'
   devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
