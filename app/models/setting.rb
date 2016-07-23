c25b0da84990d6d54e240c7dd374661a6433431b
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-23 19:01:14 +1000

    Add all the settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: settings
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  location_id :integer
+#  bio         :text
+#  learn_one   :integer
+#  learn_two   :integer
+#  learn_three :integer
+#  teach_one   :integer
+#  teach_two   :integer
+#  teach_three :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Setting < ActiveRecord::Base
+  has_many :skills
 end
