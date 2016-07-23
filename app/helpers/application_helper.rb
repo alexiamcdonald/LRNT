96080e26ede5c4dd70b5a2cd09060d9c6575af97
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-23 19:00:49 +1000

    Helper to sort lists and shit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def model_options(model_name, selected=nil, options={})
+      if options[:option_title].presence || false
+        opts = model_name.list_order.collect{|item| ["#{item.try(:option_title).presence || item.name}", item.id]}
+      else
+        opts = model_name.list_order.collect{|item| [item.name, item.id]}
+      end
+      options_for_select(opts, selected)
+    end
 end
