c114115790224e66fdf6e4ba37c84c5afe1c25a7
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-23 21:27:25 +1000

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723095850) do
+ActiveRecord::Schema.define(version: 20160723110839) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20160723095850) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
