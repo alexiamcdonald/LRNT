473ad665ae65214a3aa9d93093119862af04c3cf
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-23 14:48:19 +1000

    add auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723043614) do
+ActiveRecord::Schema.define(version: 20160723044700) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +33,8 @@ ActiveRecord::Schema.define(version: 20160723043614) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
