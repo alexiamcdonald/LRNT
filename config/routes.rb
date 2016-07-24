7e260b6cdc06f556a84b3b1a43d7b8414cbbe02b
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-24 11:43:03 +1000

    I hate devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   resources :profiles
   root 'settings#new'
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :settings
-  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
